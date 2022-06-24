465056421cbad545cb22e050a7cc180db9240a7d
Boss2021-eng <77675650+Boss2021-eng@users.noreply.github.com>
2022-06-24 09:01:27 +0100

    initial_commit

---
+++ live.js
@@ -0,0 +1,10 @@
+let my_name = "Damilola Olatayo"
+let my_height = "160cm"
+let my_country = "Nigeria"
+
+str1 = " My name is " + my_name
+str2 = " My height is " + my_height
+str3 = " My country is " + my_country
+
+
+alert( str1 + "\n" + str2 + "\n" + str3)
